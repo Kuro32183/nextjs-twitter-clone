d2164e2e30190b17e193b6d8b92b00b624b26e6d
Kuro32183 <92940109+Kuro32183@users.noreply.github.com>
2022-12-07 10:51:16 +0900

    Update Widget.tsx

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -4,7 +4,7 @@ import { TwitterTimelineEmbed } from "react-twitter-embed";
 
 function Widget() {
   return (
-    <div className="col-span-2 w-full mt-2 hidden items-start px-2 lg:inline">
+    <div className="col-span-2 w-full mt-2 hidden items-start px-2 md:inline">
       {/* Search */}
       <div className="flex items-center space-x-2 mb-5 rounded-full bg-gray-100 p-3">
         <AiOutlineSearch className="h-5 w-5 flex-shrink-0 text-gray-400" />
